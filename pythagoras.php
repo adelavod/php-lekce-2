755215cc029a8c8261569100334964ec28295e86
AdelaVodvarkova <vod.adela@gmail.com>
2019-02-25 16:35:24 +0100

    roztrideni podle cviceni

---
+++ pythagoras.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: AdelaVodvarkova
+ * Date: 2019-02-25
+ * Time: 16:27
+ */
+$a = '4';
+$b = '5';
+$obdelnikobsah = $a * $b
+    ?>
+
+<h1>Obsah obdélníku o stranách <?php echo $a ?> cm a <?php echo $b ?> cm je <?php echo $obdelnikobsah ?> cm2.</h1>
