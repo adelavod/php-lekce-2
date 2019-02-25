56a15bc694027cfa8b8743fd9ce248539308a0d1
AdelaVodvarkova <vod.adela@gmail.com>
2019-02-25 07:45:17 +0100

    roztrideni podle cviceni

--- cviceni-4.php
+++ cviceni-4.php
@@ -1,20 +1,69 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: AdelaVodvarkova
- * Date: 2019-02-21
- * Time: 20:07
- */
-$castkaBezDph = 149.99;
-$dph = 21;
-$castkaSdph = $castkaBezDph * (1 + $dph / 100);
+$titulek = 'Uživatelský profil';
+$pole = ['jméno' => 'Adéla', 'příjmení' => 'Vodvářková'];
+$pole = ['věk' => '24'];
 
-echo $castkaSdph;
+$jmeno = "Adéla";
+$poradi = 5;
+
+$cislo = 10;
+$negcislo = -2;
+$descislo = 0.1;
+
+?>
+
+
+<?php
+$promenna = true;
+var_dump($promenna);
+
 echo "<br>";
 
-var_dump($castkaBezDph);
-echo "<br>";
-var_dump($castkaSdph);
-echo "<br>";
-var_dump($dph);
+$promenna1 = 'hello ';
+$promenna2 = "world";
+var_dump($promenna1 . $promenna2 . '!');
 
+echo "<br>";
+
+echo 'jméno: ' . $jmeno . ', pořadí: ' . $poradi;
+echo "<br>";
+var_dump ('jméno: ' . $jmeno . ', pořadí: ' . $poradi);
+echo "<br>";
+$soucet1 = $cislo + $negcislo;
+echo "součet 10 + (- 2) je = $soucet1" ;
+echo "<br>";
+$soucet2 = $cislo + $descislo;
+echo "součet 10 + 0.1 je = $soucet2";
+echo "<br>";
+$nasobekvseho = $cislo * $descislo * $negcislo;
+echo "násobek všeho je $nasobekvseho";
+echo "<br>";
+var_dump($soucet1);
+echo "<br>";
+var_dump($soucet2);
+echo "<br>";
+var_dump($nasobekvseho);
+echo "<br>";
+var_dump($negcislo);
+?>
+<br>
+<h2>Výpočet DPH.</h2>
+<?php
+$castkabezDPH = 2000;
+$DPH = 21;
+$castkasDPH = $castkabezDPH + ($castkabezDPH/100)*21;
+echo "<br>";
+echo "Částka $castkabezDPH s DPH je $castkasDPH";
+echo "What is happening" ;
+echo "<br>";
+
+
+?>
+
+<hr>
+
+</main><!-- /.container -->
+
+<footer class="container-fluid">
+    <p>Vyrobeno v únoru </p>
+</footer>
--- ukol_pole.php
+++ ukol_pole.php
@@ -116,6 +116,7 @@ $descislo = 0.1;
                 <td> <?php
                 $profil['konicky']=['PHP','PS4', 'PRÁVO','fighting patriarchy'];
                 $profil['konicky'][]='memes';
+                $profil['konicky'][]='vodka';
                 print_r($profil['konicky']);
 
                 ?> </td>
@@ -125,52 +126,6 @@ $descislo = 0.1;
         <p class="lead">text.<br>text.</p>
     </div>
 
-<?php
-$promenna = true;
-var_dump($promenna);
-
-echo "<br>";
-
-$promenna1 = 'hello ';
-$promenna2 = "world";
-var_dump($promenna1 . $promenna2 . '!');
-
-echo "<br>";
-
-echo 'jméno: ' . $jmeno . ', pořadí: ' . $poradi;
-echo "<br>";
-var_dump ('jméno: ' . $jmeno . ', pořadí: ' . $poradi);
-echo "<br>";
-$soucet1 = $cislo + $negcislo;
-echo "součet 10 + (- 2) je = $soucet1" ;
-echo "<br>";
-$soucet2 = $cislo + $descislo;
-echo "součet 10 + 0.1 je = $soucet2";
-echo "<br>";
-$nasobekvseho = $cislo * $descislo * $negcislo;
-echo "násobek všeho je $nasobekvseho";
-echo "<br>";
-var_dump($soucet1);
-echo "<br>";
-var_dump($soucet2);
-echo "<br>";
-var_dump($nasobekvseho);
-echo "<br>";
-var_dump($negcislo);
-?>
-    <br>
-<h2>Výpočet DPH.</h2>
-<?php
-$castkabezDPH = 2000;
-$DPH = 21;
-$castkasDPH = $castkabezDPH + ($castkabezDPH/100)*21;
-echo "<br>";
-echo "Částka $castkabezDPH s DPH je $castkasDPH";
-echo "What is happening" ;
-echo "<br>";
-
-
-?>
 
 <hr>
 
