469b5c47a228f47559d8e2f7a98bcb086c97675b
AdelaVodvarkova <vod.adela@gmail.com>
2019-04-16 15:38:38 +0200

    birds jumbotron working

--- index.php
+++ index.php
@@ -1,75 +1,38 @@
 <!doctype html>
 <html lang="en">
   <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="icon" href="favicon.ico">
-
     <title>Starter Template for Bootstrap</title>
-
     <!-- Bootstrap core CSS -->
       <link href="css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Custom styles for this template -->
-      <link href="starter-template.css" rel="stylesheet">
+      <link href="css/adelastyle.css" rel="stylesheet" type="text/css">
   </head>
 
   <body>
+<div class="jumbotron-fluid"
+     style="margin-left: 10px;
+     margin-right: 10px;
+     border: #4e555b 5px;">
 
-    <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
-      <a class="navbar-brand" href="#">Navbar</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
+    <div class="jumbotron-fluid"
+         style="
+         margin: 0;
+         padding-top: 20%;
+         padding-bottom: 20%;
+         background-image: url('./images/erasebirds.jpeg');
+         background-size: cover">
 
-      <div class="collapse navbar-collapse" id="navbarsExampleDefault">
-        <ul class="navbar-nav mr-auto">
-          <li class="nav-item active">
-            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-          </li>
-        </ul>
-        <form class="form-inline my-2 my-lg-0">
-          <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
-          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-        </form>
-      </div>
+        <h1 style="color: whitesmoke" align="center">Zajímá nás Váš příběh.</h1>
 
-    </nav>
-    <div class="row" id="box-search">
-        <div class="img-thumbnail text-center">
-            <img src="js/photos/editedbirds%201.jpg" class="mx-auto d-block" alt="Responsive image" style="width: 100%">
-<div class="carousel-caption">
-    <p>LOREM IPSUM</p>
-</div>
-
-        </div>
     </div>
 
 
-    <hr>
-    <main role="main" class="jumbotron">
+        OLALALALA
+    </div>
 
-      <div class="jumbotron-fluid">
-        <h1>
-<?php
-  echo 'Centrální obrazovka všehomíra';
 
-?>
 
-        </h1>
-        <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
-      </div>
 
-    </main><!-- /.container -->
 
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script>window.jQuery || document.write('<script src="js/jquery-slim.min.js"><\/script>')</script>
-    <script src="js/popper.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
+
   </body>
 </html>
