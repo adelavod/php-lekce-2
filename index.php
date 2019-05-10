c1af0914b62c05457299318d0d7737ce5d7d9af1
AdelaVodvarkova <vod.adela@gmail.com>
2019-05-10 15:19:13 +0200

    navbar a background image working

--- index.php
+++ index.php
@@ -8,10 +8,34 @@
   </head>
 
   <body>
-<div class="jumbotron-fluid"
-     style="margin-left: 10px;
-     margin-right: 10px;
-     border: #4e555b 5px;">
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="#">§ AK Jordák, Raba a partneři</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+              <li class="nav-item active">
+                  <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+              </li>
+              <li class="nav-item">
+                  <a class="nav-link" href="justice.html">Justice</a>
+              </li>
+              <li class="nav-item">
+                  <a class="nav-link" href="#">Option3</a>
+              </li>
+              <li class="nav-item">
+                  <a class="nav-link" href="#">Option4</a>
+              </li>
+          </ul>
+          <form class="form-inline my-2 my-lg-0">
+              <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+              <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+          </form>
+      </div>
+  </nav>
+<div class="jumbotron-fluid">
 
     <div class="jumbotron-fluid"
          style="
@@ -19,13 +43,16 @@
          padding-top: 20%;
          padding-bottom: 20%;
          background-image: url('./images/erasebirds.jpeg');
-         background-size: cover">
-
-        <h1 style="color: whitesmoke" align="center">Zajímá nás Váš příběh.</h1>
-
+         background-size: cover;">
+           <div class="container col-md-4" id="infocontainer" style="align-self: center; opacity: 0.6; ">
+            <p style="text-align: center; font-size: 3rem; color: black">Zajímá nás Váš příběh.</p>
+               
+        </div>
     </div>
-
-
+    <div class="jumbotron-fluid" style="background-color: white">
+        <h2>Kdo jsme</h2>
+        <p>Jsme advokáti se zkušeností z mnoha oborů.</p>
+    </div>
         OLALALALA
     </div>
 
